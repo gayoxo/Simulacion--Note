fdf7a741fe954ca905d2c88e17eae7eb19e2b0fd
Joaquin Gayoso Cabada <gayoxo@gmail.com>
2012-01-17 19:21:25 +0100

    Mas mas

---
+++ AIProyect011211/src/StudentsWithUI.java
@@ -0,0 +1,16 @@
+
+import sim.engine.*;
+import sim.display.*;
+public class StudentsWithUI extends GUIState {
+public static void main(String[] args) {
+StudentsWithUI vid = new StudentsWithUI();
+Console c = new Console(vid);
+c.setVisible(true);
+}
+public StudentsWithUI() {
+super(new Curso(System.currentTimeMillis())); }
+public StudentsWithUI(SimState state) { super(state); }
+public static String getName() {
+return "Simulacion de anotador";
+}
+}
